a657ccfe69b071c68e8c1d8df2c1c6cbebee5d72
Ramón Martínez <ramartinez@aidacanarias.com>
2024-04-04 12:58:25 +0200

    kata fizz buzz completed

--- fizz-buzz/FizzBuzz.Tests/FizzBuzzTest.cs
+++ fizz-buzz/FizzBuzz.Tests/FizzBuzzTest.cs
@@ -1,13 +1,56 @@
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace FizzBuzz.Tests
 {
     public class FizzBuzzTest
     {
-        [Test]
-        public void My_First_Test()
+        [TestCase(0, "1")]
+        [TestCase(1, "2")]
+        public void PositionShouldHaveNumber(int index, string number)
         {
-            Assert.That(false, Is.True);
+            var sut = new FizzBuzz();
+
+            var result = sut.Execute();
+
+            result[index].Should().Be(number);
         }
+
+        [TestCase(2)]
+        [TestCase(5)]
+        public void MultipleOfThreeShouldBeFizz(int index)
+        {
+            var sut = new FizzBuzz();
+
+            var result = sut.Execute();
+
+            result[index].Should().Be("Fizz");
+        }
+
+        [TestCase(4)]
+        [TestCase(9)]
+        public void MultipleOfFifthShouldBeBuzz(int index)
+        {
+            var sut = new FizzBuzz();
+
+            var result = sut.Execute();
+
+            result[index].Should().Be("Buzz");
+        }
+
+
+        [TestCase(14)]
+        [TestCase(29)]
+        public void MultipleOfFifteenShouldBeFizzBuzz(int index)
+        {
+            var sut = new FizzBuzz();
+
+            var result = sut.Execute();
+
+            result[index].Should().Be("FizzBuzz");
+        }
+
+
+
     }
 }
--- fizz-buzz/FizzBuzz/FizzBuzz.cs
+++ fizz-buzz/FizzBuzz/FizzBuzz.cs
@@ -1,8 +1,36 @@
-﻿using System;
-
-namespace FizzBuzz
+﻿namespace FizzBuzz
 {
     public class FizzBuzz
     {
+        private const int Size = 100;
+        private const string Fizz = "Fizz";
+        private const string Buzz = "Buzz";
+        private const string FizzBuzzT = "FizzBuzz";
+
+        public string[] Execute()
+        {
+            var array = new string[Size];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ((i + 1) % 15 == 0)
+                {
+                    array[i] = FizzBuzzT;
+                }
+                else if ((i + 1) % 5 == 0)
+                {
+                    array[i] = Buzz;
+                }
+                else if ((i + 1) % 3 == 0)
+                {
+                    array[i] = Fizz;
+                }
+                else
+                {
+                    array[i] = (i + 1).ToString();
+                }
+            }
+            return array;
+        }
     }
 }
